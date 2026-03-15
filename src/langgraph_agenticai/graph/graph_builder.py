93107d4861c8bd295e1a6fa3350912927e31d462
rohan <mailtorohan.97@gmail.com>
2026-03-15 21:05:19 +0530

    display messages implementations

--- src/langgraph_agenticai/graph/graph_builder.py
+++ src/langgraph_agenticai/graph/graph_builder.py
@@ -1,20 +1,32 @@
 from src.langgraph_agenticai.state.state import State
 from langgraph.graph import StateGraph
 from langgraph.graph import StateGraph, START, END
+from src.langgraph_agenticai.nodes.basic_chatbot_node import BasicChatbotNode
+
 
 
 class GraphBuilder:
     def __init__(self, model):
-        self.model = model
+        self.llm = model
         self.graph_builder = StateGraph(State)
 
     def basic_chatbot_build_graph(self):
         """
         Basic chatbot graph builder. 
         """
-        self.graph_builder.add_node("chatbot","")
+        self.basic_chatbot_node = BasicChatbotNode(self.llm)
+
+        self.graph_builder.add_node("chatbot",self.basic_chatbot_node.process)
         self.graph_builder.add_edge(START, "chatbot")
         self.graph_builder.add_edge("chatbot", END)
+
+    def setup_graph(self):
+        """
+        Setup the graph.
+        """
+        if usecase == "basic_chatbot":
+            self.basic_chatbot_build_graph()
+        
         
         
 
--- src/langgraph_agenticai/main.py
+++ src/langgraph_agenticai/main.py
@@ -1,6 +1,11 @@
 import streamlit as st
 from src.langgraph_agenticai.ui.streamlitui.loadui import LoadStreamlitUI
 from langchain_groq import ChatGroq
+from src.langgraph_agenticai.llms.groqllm import GroqLLM
+from src.langgraph_agenticai.graph.graph_builder import GraphBuilder, setup_graph 
+from src.langgraph_agenticai.ui.streamlitui.display_results import DisplayResultStreamlit
+
+
 
 def load_langgraph_agenticai_app():
     """Load the LangGraph AgenticAI app.
@@ -12,3 +17,29 @@ def load_langgraph_agenticai_app():
         st.error("Failed to load user input from UI.")
     
     user_message = st.chat_input("enter your message here...")
+    if user_message:
+        try:
+            ## configure the LLM model here
+            obj_llm_config = GroqLLM(user_controls_input=user_input)
+            model = obj_llm_config.get_llm_model()
+            if not model:
+                st.error("Failed to load LLM model.")
+                return
+            usecase = user_input.get("select_usecase")
+            if not usecase:
+                st.error("Please select a use case.")
+                return
+
+            ## graph builder
+            graph_builder = GraphBuilder(model=model)
+            try:
+                graph = graph_builder.setup_grapg(usecase=usecase)
+                DisplayResultStreamlit(usecase=usecase, graph=graph, user_message=user_message).display_result_on_ui()
+            except Exception as e:
+                st.error(f"Error in initializing LangGraph AgenticAI: {e}")
+                return
+            
+        
+        except Exception as e:
+            st.error(f"Error in initializing LangGraph AgenticAI: {e}")
+            return
